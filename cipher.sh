619ec4eef2fe30d45ea8fd8cf107491f7166e359
elsstrange <ellenabstrange@gmail.com>
2020-07-13 20:38:32 +0000

    New file: cipher.sh Handles file extensions for output of use_cipher.rb Currently non-functional, as calling use_cipher.rb within bash script produces a permission error.

---
+++ cipher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$2" == "enc" ]; then
+    # For encrypting files, return a new file with the .enc extension
+    destination_file="$3.enc"
+elif [ "$2" == "dec" ]; then 
+    # For decrypting files, return a new file with the .enc extension removed
+    destination_file=${3:0:-4}
+else
+    echo "Unexpected argument. enc or dec must be specified."
+    exit 1
+fi
+
+#echo $destination_file
+
+#./cipher.rb "$@" > "$destination_file"
